d45a991285e387cf7c4b759e8ceffbb93f4ba966
srysis <87240150+srysis@users.noreply.github.com>
2021-11-25 18:21:52 +0200

    Initial commit

---
+++ test/controllers/games_controller_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+class GamesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @game = games(:one)
+  end
+
+  test "can't delete game in cart" do
+    assert_difference('Game.count', 0) do
+      delete game_url(games(:two))
+    end
+    assert_redirected_to games_url
+  end
+
+  test "should get index" do
+    get games_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_game_url
+    assert_response :success
+  end
+
+  test "should create game" do
+    assert_difference('Game.count') do
+      post games_url, params: { game: { description: @game.description, genre: @game.genre, image: @game.image, price: @game.price, title: @game.title } }
+    end
+
+    assert_redirected_to game_url(Game.last)
+  end
+
+  test "should show game" do
+    get game_url(@game)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_game_url(@game)
+    assert_response :success
+  end
+
+  test "should update game" do
+    patch game_url(@game), params: { game: { description: @game.description, genre: @game.genre, image: @game.image, price: @game.price, title: @game.title } }
+    assert_redirected_to game_url(@game)
+  end
+
+  test "should destroy game" do
+    assert_difference('Game.count', -1) do
+      delete game_url(@game)
+    end
+
+    assert_redirected_to games_url
+  end
+end
